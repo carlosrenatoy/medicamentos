bed67c9e3b3712d4ef810285f9a7b954e2b32d7c
carlosrenatoy <renatoepm70@gmail.com>
2026-05-04 12:59:01 -0300

    fix: wrap app with error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import AppIntegrated from './AppIntegrated.tsx';
 import {AuditFloatingPanel} from './components/AuditFloatingPanel.tsx';
+import {AppErrorBoundary} from './components/AppErrorBoundary.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppIntegrated />
-    <AuditFloatingPanel />
+    <AppErrorBoundary>
+      <AppIntegrated />
+      <AuditFloatingPanel />
+    </AppErrorBoundary>
   </StrictMode>,
 );
